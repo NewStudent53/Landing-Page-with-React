bbd9dabfd8f9151201eb8ffc185813cb4ca3222d
@gsrd2803 <gsrd2803@gmail.com>
2023-07-12 20:33:02 -0400

    initial commit

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 // include your styles into the webpack bundle
-import "../styles/index.css";
+import "../styles/index.scss";
+import "../styles/custom.css";
 
 //import your own components
 import Home from "./component/home.jsx";
